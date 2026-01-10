ee3384f464b2f04898f1f1be7aa77673f5bf4b48
aditiyakashyap <kashyap.aditiya27@gmail.com>
2026-01-11 01:54:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+// vite.config.js
+export default {
+  base: '/',
+  // ...other config
+}
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
